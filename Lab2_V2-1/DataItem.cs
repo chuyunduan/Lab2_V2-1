38afb4a25fab129a625e1f0c4bbfd7580bef348b
chuyunduan <chuyunduan@gmail.com>
2020-11-23 21:13:12 +0300

    添加项目文件。

---
+++ Lab2_V2-1/DataItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace Lab1_V2
+{
+    struct DataItem
+    {
+        public Vector2 Vector { get; set;}
+        public Complex Complex { get; set; }
+
+        public DataItem(Vector2 vector, Complex complex)
+        {
+            Vector = vector;
+            Complex = complex;
+        }
+
+        public override string ToString()
+        {
+            return $"Vector: { Vector.X.ToString() },{ Vector.Y.ToString() } | Complex: { Complex.ToString() }\n";
+        }
+
+        public string ToString(string format)
+        {
+            return $"Vector: { Vector.X.ToString(format) }, { Vector.Y.ToString(format) } | Complex: { Complex.ToString(format) }\n";
+        }
+    }
+}
---
+++ Lab2_V2-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Lab1_V2
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            try
+            {
+                V2DataOnGrid grid = new V2DataOnGrid("infofile.txt");
+                Console.WriteLine(grid.ToLongString("n"));
+            }
+            catch (Exception ex)//4
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            //3
+            try
+            {
+                V2MainCollection mainCollection = new V2MainCollection();
+                mainCollection.AddDefaults();
+                Console.WriteLine(mainCollection.ToLongString("n"));
+                
+                Console.WriteLine($"Average V2MainCollection: { mainCollection.Avg.ToString() }\n");
+
+                Console.WriteLine($"Near average: { mainCollection.NearAvg.ToString() }\n");
+
+                Console.WriteLine("Vector2:");
+                foreach (Vector2 v in mainCollection.Vectors)
+                {
+                    Console.WriteLine(v.ToString());
+                }
+            }
+            catch (Exception ex)//4
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
